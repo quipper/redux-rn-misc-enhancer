0902f05f91eb24a7bca76350dee098370845559f
hotchemi <hot.chemistry21@gmail.com>
2018-07-17 02:02:39 +0900

    Add example project.

---
+++ example/README.md
@@ -0,0 +1,7 @@
+# react-native-i18n-ts example
+
+```sh
+npm install
+npm i18n-ts # or i18n-ts:watch
+npm start:ios # or start:android
+```
---
+++ example/src/App.tsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\nCmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\nShake or press menu button for dev menu',
+});
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>{'example'}</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
+  }
+}
